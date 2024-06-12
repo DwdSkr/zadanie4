62a8b10c7ae63c6fe4db7907fce9d8589fd7cea3
DwdSkr <166405578+DwdSkr@users.noreply.github.com>
2024-06-12 13:06:03 +0200

    12.06.2024
    Działa, do usprawnienia tworzenie i usuwanie elementów.

--- script.js
+++ script.js
@@ -3,60 +3,52 @@ function test() {
 	const newTask = document.getElementById(`newTask`).value;
 	if (newTask !== ``) {
 		const newLi = document.createElement(`li`);
+		newLi.innerText = newTask;
 		//
 		const btnEdit = document.createElement(`button`);
-		btnEdit.textContent = `edytuj`;
-		btnEdit.setAttribute(`onclick`, `editFunction()`);
-		//
 		const btnDelete = document.createElement(`button`);
-		btnDelete.textContent = `usuń`;
-		btnDelete.setAttribute(`onclick`, `deleteFunction()`);
+		let taskContainer = newTask;
 		//
-		newLi.innerText = newTask;
+		btnEdit.textContent = `edytuj`;
+		btnDelete.textContent = `usuń`;
+		//
+		btnEdit.addEventListener(`click`, () => {
+			newLi.textContent = ``;
+			//
+			const changeTaskInput = document.createElement(`input`);
+			changeTaskInput.type = `text`;
+			changeTaskInput.id = `changeTask`;
+			changeTaskInput.value = taskContainer;
+			newLi.appendChild(changeTaskInput);
+			//
+			const changeTaskButton = document.createElement(`button`);
+			changeTaskButton.innerText = `Zatwierdź zmiany`;
+			changeTaskButton.addEventListener('click', () => {
+				const changedTask = document.getElementById(`changeTask`).value;
+				if (changedTask !== '') {
+					taskContainer = changedTask;
+					newLi.textContent = changedTask;
+					newLi.appendChild(btnEdit);
+					newLi.appendChild(btnDelete);
+				} else {
+					alert(`Nazwa zadania nie może być pusta.`);
+				}
+			});
+			newLi.appendChild(changeTaskButton);
+			newLi.appendChild(btnDelete);
+		});
+		//
+		btnDelete.addEventListener(`click`, () => {
+			newLi.parentNode.removeChild(newLi);
+		});
+		//
+
 		parent.appendChild(newLi);
-		const liManagement = document.querySelectorAll(`#list li`);
-		liManagement[liManagement.length - 1].appendChild(btnEdit);
-		liManagement[liManagement.length - 1].appendChild(btnDelete);
+		newLi.appendChild(btnEdit);
+		newLi.appendChild(btnDelete);
 		//
 		document.getElementById(`newTask`).value = ``;
 	} else {
 		alert(`Nazwa zadania nie może być pusta.`);
 	}
-}
-
-function deleteFunction() {
-	const deleteItem = document.querySelector(`li`);
-	parent.removeChild(deleteItem);
-}
-function editFunction() {
-	const changeTaskInput = document.createElement(`input`);
-	changeTaskInput.type = `text`;
-	changeTaskInput.id = `changeTask`;
-	//
-	const changeTaskButton = document.createElement(`button`);
-	changeTaskButton.setAttribute(`onclick`, `changeTask()`);
-	changeTaskButton.innerText = `Zatwierdź zmiany`;
-	//
-	const removeEdit = document.querySelector(`#list li button`);
-	removeEdit.parentNode.removeChild(removeEdit);
-	//
-	const editPlace = document.querySelector(`#list li`);
-	//
-
-	editPlace.insertBefore(
-		changeTaskInput,
-		document.querySelector(`#list li button`)
-	);
-	editPlace.insertBefore(
-		changeTaskButton,
-		document.querySelector(`#list li button`)
-	);
-	//
-}
-function changeTask() {
-	// const changeTaskValue = document.getElementById(`changeTask`).value;
-	// if (changeTaskValue !== ``) {
-	// } else {
-	// 	alert(`Nazwa zadania nie może być pusta.`);
-	// }
 }
